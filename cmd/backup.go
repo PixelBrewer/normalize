8b1dc377e0ab8aa6392a6d153f63b57be779de5f
Alex <alex@Vilya.local>
2026-03-08 22:40:30 -0700

    Adding some basic backup command starter code

--- cmd/backup.go
+++ cmd/backup.go
@@ -1,1 +1,12 @@
-// TODO: Implement backup.go  
+// TODO: Implement backup.go
+// Module cmd is for command definitions
+package cmd
+
+import (
+	"fmt"
+)
+
+func getFileTypeToBackup() string {
+	fmt.Println("Please provide a filetype to backup.")
+	fileTypeScanner := bufio.scann()
+}
